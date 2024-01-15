254c6aee3773d897ca28b47cf076b39906de6bf1
OSCAR JOSE DURAN CUELLO <odurancuello@gmail.com>
2024-01-14 22:19:00 -0400

    Add vaccines in pet dashboard

--- lib/widgets/pets/latest_appointment_widget.dart
+++ lib/widgets/pets/latest_appointment_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vetplus/models/appointments_model.dart';
+import 'package:vetplus/utils/date_utils.dart';
 import 'package:vetplus/widgets/pets/dashboard_card.dart';
 
 class LatestAppointmentWidget extends StatelessWidget {
@@ -82,6 +83,91 @@ class LatestAppointmentWidget extends StatelessWidget {
                 fontSize: isTablet ? 16 : 14.sp,
                 fontFamily: 'Inter',
               )),
+          SizedBox(height: 12.sp),
+          Text(
+            AppLocalizations.of(context)!.vaccines,
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: isTablet ? 16 : 14.sp,
+              fontFamily: 'Roboto',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 4.sp),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.date,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                latestAppointment!.observations!.vaccines!.date == null ||
+                        latestAppointment!.observations!.vaccines!.date! == ''
+                    ? 'N/A'
+                    : formatDateTime(
+                        latestAppointment!.observations!.vaccines!.date!),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.vaccineBrand,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                latestAppointment!.observations!.vaccines!.vaccineBrand! == ''
+                    ? 'N/A'
+                    : latestAppointment!.observations!.vaccines!.vaccineBrand!,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.vaccineBatch,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                latestAppointment!.observations!.vaccines!.vaccineBatch! == ''
+                    ? 'N/A'
+                    : latestAppointment!.observations!.vaccines!.vaccineBatch!,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: isTablet ? 16 : 14.sp,
+                  fontFamily: 'Inter',
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
